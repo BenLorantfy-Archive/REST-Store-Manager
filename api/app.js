92bf65d383d9d51f294afbd5a5f7557542d8038b
Kyle Stevenson <ky_steve@hotmail.com>
2016-11-13 20:37:14 -0500

    add search and insert endpoints for product, order, and cart

--- api/app.js
+++ api/app.js
@@ -84,11 +84,144 @@ app.post("/customers",function(req,res){
     });
 })
 
+// [ Product Search ]
+    app.get("/products", function (req, res) {
+        var query = db
+            .select("prodID", "prodName", "price", "prodWeight", "inStock")
+            .from("Product");
+
+        if (req.query.prodID) {
+            query.orWhere("prodID", "like", "%" + req.query.prodID + "%");
+        }
+
+        if (req.query.prodName) {
+            query.orWhere("prodName", "like", "%" + req.query.prodName + "%");
+        }
+
+        if (req.query.price) {
+            query.orWhere("price", "like", "%" + req.query.price + "%");
+        }
+
+        if (req.query.prodWeight) {
+            query.orWhere("prodWeight", "like", "%" + req.query.prodWeight + "%");
+        }
+
+        if (req.query.inStock) {
+            query.orWhere("inStock", "like", "%" + req.query.inStock + "%");
+        }
+
+        // console.log(query);
+
+        query.then(function (products) {
+            res.end(JSON.stringify(products));
+        });
+    })
+
+// [ Product Insert ]
+    app.post("/products", function (req, res) {
+        var query = db
+            .insert({
+                prodName: req.body.prodName,
+                price: req.body.price,
+                prodWeight: req.body.prodWeight,
+                inStock: req.body.inStock
+            })
+            .into('Product');
+
+        query.then(function () {
+            res.end(JSON.stringify({sucess: true}));
+        });
+    })
+
+// [ Order Search ]
+    app.get("/orders", function (req, res) {
+        var query = db
+            .select("orderID", "custID", "poNumber", "orderDate")
+            .from("Order");
+
+        if (req.query.orderID) {
+            query.orWhere("orderID", "like", "%" + req.query.orderID + "%");
+        }
+
+        if (req.query.custID) {
+            query.orWhere("custID", "like", "%" + req.query.custID + "%");
+        }
+
+        if (req.query.poNumber) {
+            query.orWhere("poNumber", "like", "%" + req.query.poNumber + "%");
+        }
+
+        if (req.query.orderDate) {
+            query.orWhere("orderDate", "like", "%" + req.query.orderDate + "%");
+        }
+
+        // console.log(query);
+
+        query.then(function (orders) {
+            res.end(JSON.stringify(orders));
+        });
+    })
+
+// [ Order Insert ]
+    app.post("/orders", function (req, res) {
+        var query = db
+            .insert({
+                custID: req.body.custID,
+                poNumber: req.body.poNumber,
+                orderDate: req.body.orderDate
+            })
+            .into('Order');
+
+        query.then(function () {
+            res.end(JSON.stringify({sucess: true}));
+        });
+    })
+
+// [ Cart Search ]
+    app.get("/carts", function (req, res) {
+        var query = db
+            .select("orderID", "prodID", "quantity")
+            .from("Order");
+
+        if (req.query.orderID) {
+            query.orWhere("orderID", "like", "%" + req.query.orderID + "%");
+        }
+
+        if (req.query.prodID) {
+            query.orWhere("prodID", "like", "%" + req.query.prodID + "%");
+        }
+
+        if (req.query.quantity) {
+            query.orWhere("quantity", "like", "%" + req.query.quantity + "%");
+        }
+
+        // console.log(query);
+
+        query.then(function (carts) {
+            res.end(JSON.stringify(carts));
+        });
+    })
+
+// [ Cart Insert ]
+    app.post("/carts", function (req, res) {
+        var query = db
+            .insert({
+                orderID: req.body.orderID,
+                prodID: req.body.prodID,
+                quantity: req.body.quantity
+            })
+            .into('Cart');
+
+        query.then(function () {
+            res.end(JSON.stringify({sucess: true}));
+        });
+    })
+
 
 // [ Listen for requests ]
-app.listen(80, function () {
-  console.log('Web server listening on port 80...');
-});
+    app.listen(80, function () {
+        console.log('Web server listening on port 80...');
+    });
 
 //process.on('SIGTERM', function () {
 //  app.close(function () {
